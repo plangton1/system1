6a5cc5986242e12854644cd8eb681830f8f8e031
plangton1 <plangtonjanratsamee@gmail.com>
2021-12-02 15:29:56 +0700

    1
    1

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+
+<?php include('./include/head.php');?>
+
+<body>
+    <div class="preloader">
+        <div class="preloader-inner">
+            <div class="preloader-icon">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <!-- /End Preloader -->
+
+<?php include('./include/menu.php');?>
+
+    <!-- Start Hero Area -->
+    <section class="hero-area overlay">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 offset-lg-1 col-md-12 col-12">
+                    <div class="hero-text text-center">
+                        <!-- Start Hero Text -->
+                        <div class="section-heading">
+                            <h2 class="wow fadeInUp" data-wow-delay=".3s">ค้นหาเอกสารที่นี่</h2>
+                            <p class="wow fadeInUp" data-wow-delay=".5s"></p>
+                        </div>
+                        <!-- End Search Form -->
+                        <!-- Start Search Form -->
+                        <div class="search-form wow fadeInUp" data-wow-delay=".7s">
+                            <div class="row">
+                                <div class="col-lg-4 col-md-4 col-12 p-0">
+                                    <div class="search-input">
+                                        <label for="keyword"><i class="lni lni-search-alt theme-color"></i></label>
+                                        <input type="text" name="keyword" id="keyword" placeholder="Product keyword">
+                                    </div>
+                                </div>
+                                
+                               
+                                <div class="col-lg-2 col-md-2 col-12 p-0">
+                                    <div class="search-btn button">
+                                        <button class="btn"><i class="lni lni-search-alt"></i> Search</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Search Form -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Hero Area -->
+
+    <!-- Start Categories Area -->
+    <section class="categories">
+        <div class="container">
+            <div class="cat-inner">
+                <div class="row">
+                    <div class="col-12 p-0">
+                        <div class="category-slider">
+                            <!-- Start Single Category -->
+                            <a href="category.html" class="single-cat">
+                                <div class="icon">
+                                    <img src="assets/images/categories/car.svg" alt="#">
+                                </div>
+                                <h3>เพิ่มเอกสาร</h3>
+                                <h5 class="total">35</h5>
+                            </a>
+                            <!-- End Single Category -->
+                            <!-- Start Single Category -->
+                            <a href="category.html" class="single-cat">
+                                <div class="icon">
+                                    <img src="assets/images/categories/laptop.svg" alt="#">
+                                </div>
+                                <h3>แก้ไขสถานะ</h3>
+                                <h5 class="total">22</h5>
+                            </a>
+                            <!-- End Single Category -->
+                            <!-- Start Single Category -->
+                            <a href="category.html" class="single-cat">
+                                <div class="icon">
+                                    <img src="assets/images/categories/matrimony.svg" alt="#">
+                                </div>
+                                <h3>รายงานเอกสาร</h3>
+                                <h5 class="total">55</h5>
+                            </a>
+                            <!-- End Single Category -->
+                            
+                            <!-- End Single Category -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /End Categories Area -->
+
+    <!-- Start Items Grid Area -->
+    <section class="items-grid section custom-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title">
+                        <h2 class="wow fadeInUp" data-wow-delay=".4s">เอกสารทั้งหมด</h2>
+                        <p class="wow fadeInUp" data-wow-delay=".6s">-</p>
+                    </div>
+                </div>
+            </div>
+            
+                   
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /End Items Grid Area -->
+
+   
+    <!-- Start Footer Area -->
+    <footer class="footer">
+        <!-- Start Footer Top -->
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-12">
+                        <!-- Single Widget -->
+                        <div class="single-footer mobile-app">
+                            <h3>Mobile Apps</h3>
+                            <div class="app-button">
+                                <a href="javascript:void(0)" class="btn">
+                                    <i class="lni lni-play-store"></i>
+                                    <span class="text">
+                                        <span class="small-text">Get It On</span>
+                                        Google Play
+                                    </span>
+                                </a>
+                                <a href="javascript:void(0)" class="btn">
+                                    <i class="lni lni-apple"></i>
+                                    <span class="text">
+                                        <span class="small-text">Get It On</span>
+                                        App Store
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <!-- End Single Widget -->
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-12">
+                        <!-- Single Widget -->
+                        <div class="single-footer f-link">
+                            <h3>Locations</h3>
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-12">
+                                    <ul>
+                                        <li><a href="javascript:void(0)">Chicago</a></li>
+                                        <li><a href="javascript:void(0)">New York City</a></li>
+                                        <li><a href="javascript:void(0)">San Francisco</a></li>
+                                        <li><a href="javascript:void(0)">Washington</a></li>
+                                        <li><a href="javascript:void(0)">Boston</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-12">
+                                    <ul>
+                                        <li><a href="javascript:void(0)">Los Angeles</a></li>
+                                        <li><a href="javascript:void(0)">Seattle</a></li>
+                                        <li><a href="javascript:void(0)">Las Vegas</a></li>
+                                        <li><a href="javascript:void(0)">San Diego</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Widget -->
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-12">
+                        <!-- Single Widget -->
+                        <div class="single-footer f-link">
+                            <h3>Quick Links</h3>
+                            <ul>
+                                <li><a href="javascript:void(0)">About Us</a></li>
+                                <li><a href="javascript:void(0)">How It's Works</a></li>
+                                <li><a href="javascript:void(0)">Login</a></li>
+                                <li><a href="javascript:void(0)">Signup</a></li>
+                                <li><a href="javascript:void(0)">Help & Support</a></li>
+                            </ul>
+                        </div>
+                        <!-- End Single Widget -->
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-12">
+                        <!-- Single Widget -->
+                        <div class="single-footer f-contact">
+                            <h3>Contact</h3>
+                            <ul>
+                                <li>23 New Design Str, Lorem Upsum 10<br> Hudson Yards, USA</li>
+                                <li>Tel. +(123) 1800-567-8990 <br> Mail. support@classigrids.com</li>
+                            </ul>
+                        </div>
+                        <!-- End Single Widget -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/ End Footer Middle -->
+    </footer>
+    <!--/ End Footer Area -->
+
+    <!-- ========================= scroll-top ========================= -->
+    <a href="#" class="scroll-top btn-hover">
+        <i class="lni lni-chevron-up"></i>
+    </a>
+
+   <?php include('./include/script.php');?>
+</body>
+
+</html>
